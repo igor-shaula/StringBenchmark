cdd0b8a28b39c8a8d1e7dc48cdfd9e88de0b44f9
igor-shaula <igor.shaula@gmail.com>
2018-05-13 03:41:57 +0300

    updated compileSdkVersion, targetSdkVersion & support libraries' versions \ app is not checked for now \\

--- app/src/main/java/com/igor/shaula/string_benchmark/annotations/NN.java
+++ app/src/main/java/com/igor/shaula/string_benchmark/annotations/NN.java
@@ -1,5 +1,6 @@
 package com.igor.shaula.string_benchmark.annotations;
 
+import java.lang.annotation.Annotation;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -11,11 +12,28 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.SOURCE)
 @Target({ElementType.LOCAL_VARIABLE, ElementType.FIELD})
 public @interface NN {
+
+    boolean isNull();
 }
 
 /*
 ...
-@NN int anInstance = new AnInstance(); // cannot be null
+@NN AnInstance anInstance = new AnInstance(); // cannot be null
 @NN anInstance = instances.getByKey(key); // cannot be null
 @NN anInstance = null; // should underline with red mistake
  */
+
+class NNImpl implements NN {
+
+    @Override
+    public Class<? extends Annotation> annotationType() {
+        return NN.class;
+    }
+
+    @Override
+    public boolean isNull() {
+//        Class<? extends NNImpl> aClass = getClass();
+//        aClass.getAnnotation(NN.class);
+        return false;
+    }
+}
