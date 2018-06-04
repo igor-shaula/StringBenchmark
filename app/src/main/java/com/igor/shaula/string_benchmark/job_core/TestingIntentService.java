70b623501ced54e4aef2e2a15d066b8a92d4af0b
igor-shaula <igor.shaula@gmail.com>
2018-06-04 23:59:10 +0300

    moved all iteration job specific code into IterationsJob-class - currently inner \\

--- app/src/main/java/com/igor/shaula/string_benchmark/job_core/TestingIntentService.java
+++ app/src/main/java/com/igor/shaula/string_benchmark/job_core/TestingIntentService.java
@@ -98,7 +98,7 @@ public class TestingIntentService extends IntentService {
                 break;
             case C.Intent.ACTION_START_ALL_TESTS:
                 final int howManyIterations = intent.getIntExtra(C.Intent.NAME_ITERATIONS, 1);
-                measurePerformanceInLoop(howManyIterations);
+                new IterationsJob().measurePerformanceInLoop(howManyIterations);
                 L.w(CN, "onHandleIntent ` howManyIterations = " + howManyIterations);
                 break;
             default:
@@ -171,94 +171,101 @@ public class TestingIntentService extends IntentService {
         );
     }
 
-    private void measurePerformanceInLoop(final int howManyIterations) {
+    class IterationsJob {
 
-        final DataTransport appLink = (DataTransport) getApplication();
-        // longStringForTest may be null - but it's normally processed by all our logging variants \\
-        final String longStringForTest = appLink.getLongStringForTest();
-        final List<Long> oneIterationResults = new ArrayList<>(C.Order.VARIANTS_TOTAL);
+        public void measurePerformanceInLoop(final int howManyIterations) {
 
-        for (int i = 0; i < howManyIterations; i++) {
-            if (!appLink.isAllowedToRunIterations()) {
-                L.i(CN, "measurePerformanceInLoop ` isMarkedForStop worked -> stopping now");
-                LocalBroadcastManager.getInstance(this).sendBroadcast(new Intent(C.Intent.ACTION_JOB_STOPPED));
-                break;
-            }
-            oneIterationResults.clear();
-            // trying to exclude strange numbers for the first test method by pre-heating it \\
-            runSoutMethod(longStringForTest);
-            oneIterationResults.add(C.Order.INDEX_OF_SOUT, runSoutMethod(longStringForTest));
-            // pre-heating all other methods to avoid their slowing down for the first time invoked \\
-            runLogMethod(longStringForTest);
-            oneIterationResults.add(C.Order.INDEX_OF_LOG, runLogMethod(longStringForTest));
-            runDalMethod(longStringForTest);
-            oneIterationResults.add(C.Order.INDEX_OF_DAL, runDalMethod(longStringForTest));
-            runVal1Method(longStringForTest);
-            oneIterationResults.add(C.Order.INDEX_OF_VAL_1, runVal1Method(longStringForTest));
-            runVal2Method(longStringForTest);
-            oneIterationResults.add(C.Order.INDEX_OF_VAL_2, runVal2Method(longStringForTest));
-            runVal3Method(longStringForTest);
-            oneIterationResults.add(C.Order.INDEX_OF_VAL_3, runVal3Method(longStringForTest));
-            runSLVoidMethod(longStringForTest);
-            oneIterationResults.add(C.Order.INDEX_OF_SL_VOID, runSLVoidMethod(longStringForTest));
-            runSLIntMethod(longStringForTest);
-            oneIterationResults.add(C.Order.INDEX_OF_SL_INT, runSLIntMethod(longStringForTest));
-            // TODO: 02.06.2018 try to avoid hardcoded indexes by using auto-incremented counter \\
+            final DataTransport appLink = (DataTransport) getApplication();
+            // longStringForTest may be null - but it's normally processed by all our logging variants \\
+            final String longStringForTest = appLink.getLongStringForTest();
+            final List<Long> oneIterationResults = new ArrayList<>(C.Order.VARIANTS_TOTAL);
+
+            for (int i = 0; i < howManyIterations; i++) {
+                if (!appLink.isAllowedToRunIterations()) {
+                    L.i(CN, "measurePerformanceInLoop ` isMarkedForStop worked -> stopping now");
+                    notifyUiThatJobStopped();
+                    break;
+                }
+                oneIterationResults.clear();
+                // trying to exclude strange numbers for the first test method by pre-heating it \\
+                runSoutMethod(longStringForTest);
+                oneIterationResults.add(C.Order.INDEX_OF_SOUT, runSoutMethod(longStringForTest));
+                // pre-heating all other methods to avoid their slowing down for the first time invoked \\
+                runLogMethod(longStringForTest);
+                oneIterationResults.add(C.Order.INDEX_OF_LOG, runLogMethod(longStringForTest));
+                runDalMethod(longStringForTest);
+                oneIterationResults.add(C.Order.INDEX_OF_DAL, runDalMethod(longStringForTest));
+                runVal1Method(longStringForTest);
+                oneIterationResults.add(C.Order.INDEX_OF_VAL_1, runVal1Method(longStringForTest));
+                runVal2Method(longStringForTest);
+                oneIterationResults.add(C.Order.INDEX_OF_VAL_2, runVal2Method(longStringForTest));
+                runVal3Method(longStringForTest);
+                oneIterationResults.add(C.Order.INDEX_OF_VAL_3, runVal3Method(longStringForTest));
+                runSLVoidMethod(longStringForTest);
+                oneIterationResults.add(C.Order.INDEX_OF_SL_VOID, runSLVoidMethod(longStringForTest));
+                runSLIntMethod(longStringForTest);
+                oneIterationResults.add(C.Order.INDEX_OF_SL_INT, runSLIntMethod(longStringForTest));
+                // TODO: 02.06.2018 try to avoid hardcoded indexes by using auto-incremented counter \\
 /*
             // as this part of code is hot - no need of debug logging here during normal usage \\
             L.w("measurePerformanceInLoop", "i = " + i +
                     " oneIterationResults = " + oneIterationResults);
 */
-            appLink.transportOneIterationsResult(oneIterationResults, i + 1);
+                appLink.transportOneIterationsResult(oneIterationResults, i + 1);
+            }
+        }
+
+        private long runSoutMethod(@Nullable String longStringForTest) {
+            long soutNanoTime = System.nanoTime();
+            System.out.println(longStringForTest);
+            return System.nanoTime() - soutNanoTime;
+        }
+
+        private long runLogMethod(@Nullable String longStringForTest) {
+            long logNanoTime = System.nanoTime();
+            Log.v(CN, longStringForTest);
+            return System.nanoTime() - logNanoTime;
+        }
+
+        private long runDalMethod(@Nullable String longStringForTest) {
+            long dalNanoTime = System.nanoTime();
+            DAL.v(CN, longStringForTest);
+            return System.nanoTime() - dalNanoTime;
+        }
+
+        private long runVal1Method(@Nullable String longStringForTest) {
+            long val1NanoTime = System.nanoTime();
+            VAL1.v(longStringForTest);
+            return System.nanoTime() - val1NanoTime;
+        }
+
+        private long runVal2Method(@Nullable String longStringForTest) {
+            long val2NanoTime = System.nanoTime();
+            VAL2.v(longStringForTest);
+            return System.nanoTime() - val2NanoTime;
+        }
+
+        private long runVal3Method(@Nullable String longStringForTest) {
+            long val3NanoTime = System.nanoTime();
+            VAL3.v(longStringForTest);
+            return System.nanoTime() - val3NanoTime;
+        }
+
+        private long runSLVoidMethod(@Nullable String longStringForTest) {
+            long salNanoTime = System.nanoTime();
+            SLVoid.v(longStringForTest);
+            return System.nanoTime() - salNanoTime;
+        }
+
+        private long runSLIntMethod(@Nullable String longStringForTest) {
+            long val0NanoTime = System.nanoTime();
+            SLInt.v(longStringForTest);
+            return System.nanoTime() - val0NanoTime;
         }
     }
 
-    private long runSoutMethod(@Nullable String longStringForTest) {
-        long soutNanoTime = System.nanoTime();
-        System.out.println(longStringForTest);
-        return System.nanoTime() - soutNanoTime;
-    }
-
-    private long runLogMethod(@Nullable String longStringForTest) {
-        long logNanoTime = System.nanoTime();
-        Log.v(CN, longStringForTest);
-        return System.nanoTime() - logNanoTime;
-    }
-
-    private long runDalMethod(@Nullable String longStringForTest) {
-        long dalNanoTime = System.nanoTime();
-        DAL.v(CN, longStringForTest);
-        return System.nanoTime() - dalNanoTime;
-    }
-
-    private long runVal1Method(@Nullable String longStringForTest) {
-        long val1NanoTime = System.nanoTime();
-        VAL1.v(longStringForTest);
-        return System.nanoTime() - val1NanoTime;
-    }
-
-    private long runVal2Method(@Nullable String longStringForTest) {
-        long val2NanoTime = System.nanoTime();
-        VAL2.v(longStringForTest);
-        return System.nanoTime() - val2NanoTime;
-    }
-
-    private long runVal3Method(@Nullable String longStringForTest) {
-        long val3NanoTime = System.nanoTime();
-        VAL3.v(longStringForTest);
-        return System.nanoTime() - val3NanoTime;
-    }
-
-    private long runSLVoidMethod(@Nullable String longStringForTest) {
-        long salNanoTime = System.nanoTime();
-        SLVoid.v(longStringForTest);
-        return System.nanoTime() - salNanoTime;
-    }
-
-    private long runSLIntMethod(@Nullable String longStringForTest) {
-        long val0NanoTime = System.nanoTime();
-        SLInt.v(longStringForTest);
-        return System.nanoTime() - val0NanoTime;
+    private void notifyUiThatJobStopped() {
+        LocalBroadcastManager.getInstance(this).sendBroadcast(new Intent(C.Intent.ACTION_JOB_STOPPED));
     }
 
     @SuppressWarnings("unused")
