7a265228139be7910d604d90af55b1512f72f5a4
igor-shaula <igor.shaula@gmail.com>
2018-07-09 23:33:40 +0300

    prepared to get all system properties in log - this is experimental & not tested yet - just an idea \\

--- app/src/main/java/com/igor/shaula/string_benchmark/app_components/main_screen/MainLogic.java
+++ app/src/main/java/com/igor/shaula/string_benchmark/app_components/main_screen/MainLogic.java
@@ -18,6 +18,7 @@ import com.igor.shaula.string_benchmark.utils.U;
 
 import org.json.JSONObject;
 
+import java.util.Enumeration;
 import java.util.List;
 import java.util.Map;
 
@@ -362,5 +363,42 @@ public final class MainLogic implements MainHub.LogicLink {
         SLInt.o(null);
         SLInt.pV(null, null);
         SLInt.pV("multiple ps", "_+_");
+
+        Enumeration<?> properties = System.getProperties().propertyNames();
+        while (properties.hasMoreElements()) {
+            SLInt.isV("property", properties.nextElement());
+        }
+/*
+    // add into info screen - from java.lang.System \ getProperties() \\
+    Key                             Description of Associated Value
+    java.version	                Java Runtime Environment version
+    java.vendor	                    Java Runtime Environment vendor
+    java.vendor.url	                Java vendor URL
+    java.home	                    Java installation directory
+    java.vm.specification.version	Java Virtual Machine specification version
+    java.vm.specification.vendor	Java Virtual Machine specification vendor
+    java.vm.specification.name	    Java Virtual Machine specification name
+    java.vm.version	                Java Virtual Machine implementation version
+    java.vm.vendor	                Java Virtual Machine implementation vendor
+    java.vm.name	                Java Virtual Machine implementation name
+    java.specification.version	    Java Runtime Environment specification version
+    java.specification.vendor	    Java Runtime Environment specification vendor
+    java.specification.name	        Java Runtime Environment specification name
+    java.class.version	            Java class format version number
+    java.class.path	                Java class path
+    java.library.path	            List of paths to search when loading libraries
+    java.io.tmpdir	                Default temp file path
+    java.compiler	                Name of JIT compiler to use
+    java.ext.dirs	                Path of extension directory or directories
+    os.name	                        Operating system name
+    os.arch	                        Operating system architecture
+    os.version	                    Operating system version
+    file.separator	                File separator ("/" on UNIX)
+    path.separator	                Path separator (":" on UNIX)
+    line.separator	                Line separator ("\n" on UNIX)
+    user.name	                    User's account name
+    user.home	                    User's home directory
+    user.dir	                    User's current working directory
+*/
     }
 }
