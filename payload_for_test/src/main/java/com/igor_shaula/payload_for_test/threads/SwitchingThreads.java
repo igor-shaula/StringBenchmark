8e6f9e78c9fe0a364e36ad3670dc104bf8c4cb5d
igor-shaula <igor.shaula@gmail.com>
2018-09-19 23:15:01 +0300

    added new class for continuation of experiments with my custom api usage comfort & simplicity, also formatted SwitchingThreads \\

--- payload_for_test/src/main/java/com/igor_shaula/payload_for_test/threads/SwitchingThreads.java
+++ payload_for_test/src/main/java/com/igor_shaula/payload_for_test/threads/SwitchingThreads.java
@@ -3,22 +3,24 @@ package com.igor_shaula.payload_for_test.threads;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
+import com.igor_shaula.base_utils.annotations.TypeDoc;
+
 import java.util.ArrayList;
 
-//@TypeDoc(createdBy = "shaula", createdOn = "18.07.2018", purpose = "" +
-//        "test switching between threads when working with collections")
+@TypeDoc(createdBy = "shaula", createdOn = "18.07.2018", purpose = "" +
+        "test switching between threads when working with collections")
 public class SwitchingThreads {
-
+    
     @Nullable
     private static SwitchingThreads instance;
     @NonNull
     private ArrayList<String> testedArrayList = new ArrayList<>(10);
     @NonNull
     private Callback callback;
-
+    
     private SwitchingThreads(@NonNull Callback callback) {
         this.callback = callback;
-
+        
         testedArrayList.add("0");
         testedArrayList.add("1");
         testedArrayList.add("2");
@@ -30,7 +32,7 @@ public class SwitchingThreads {
         testedArrayList.add("8");
         testedArrayList.add("9");
     }
-
+    
     @NonNull
     public static SwitchingThreads getInstance(@NonNull Callback callback) {
         if (instance == null) {
@@ -38,7 +40,7 @@ public class SwitchingThreads {
         }
         return instance;
     }
-
+    
     public void iterateThroughArrayListInThisThread() {
         final StringBuilder resultBuilder = new StringBuilder();
         for (String s : testedArrayList) {
@@ -46,7 +48,7 @@ public class SwitchingThreads {
         }
         callback.delegateResult(resultBuilder.toString());
     }
-
+    
     public void iterateThroughArrayListInNewThread() {
         final Thread workerThread = new Thread(new Runnable() {
             @Override
@@ -56,9 +58,9 @@ public class SwitchingThreads {
         });
         workerThread.start();
     }
-
+    
     public interface Callback {
-
+        
         void delegateResult(@Nullable String result);
     }
 }
